e51030f3c141ba7be278ff6d3f57f1b8409e0df8
Grzegorz Ciepiel <grzegorz@ciepiel.pl>
2024-02-19 11:31:13 +0100

    First commit, new db - twich, add table stream

---
+++ main.py
@@ -0,0 +1,27 @@
+import sqlite3
+import csv
+
+db = sqlite3.connect('twich.db')
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE stream (
+        'time', 'device_id', 'login', 'channel', 'country', 'player', 'game', 'stream_format', 'subscriber');
+        ''')
+with open('stream.csv','r') as data:
+    dr = csv.DictReader(data)
+    to_db = [(i['time'],
+              i['device_id'],
+              i['login'],
+              i['channel'],
+              i['country'],
+              i['player'],
+              i['game'],
+              i['stream_format'],
+              i['subscriber']) for i in dr]
+
+cursor.executemany('''
+    INSERT INTO stream ('time', 'device_id', 'login', 'channel', 'country', 'player', 'game', 'stream_format', 'subscriber')
+    VALUES (?,?,?,?,?,?,?,?,?);''', to_db)
+
+db.commit()
+db.close()
